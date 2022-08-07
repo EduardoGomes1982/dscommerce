c289b63a1c88eb3a38290541d450423d6d8114f6
EduardoGomes1982 <edu1982talk@gmail.com>
2022-08-07 00:24:58 -0300

    Order Entity

--- backend/src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -1,12 +1,15 @@
 package com.devsuperior.dscommerce.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,6 +23,9 @@ public class User {
 	private String phone;
 	private LocalDate birthDate;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
