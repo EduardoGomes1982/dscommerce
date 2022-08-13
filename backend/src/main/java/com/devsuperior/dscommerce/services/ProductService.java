2497763cfa7b80765caa156784951b009dd19960
EduardoGomes1982 <edu1982talk@gmail.com>
2022-08-13 00:59:42 -0300

    Post Product

--- backend/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -25,10 +25,17 @@ public class ProductService {
 		Product product = result.get();
 		return new ProductDTO(product);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAll(Pageable pageable) {
 		Page<Product> result = productRepository.findAll(pageable);
 		return result.map(p -> new ProductDTO(p));
+	}
+
+	@Transactional(readOnly = false)
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product(null, dto.getName(), dto.getDescription(), dto.getPrice(), dto.getImgUrl());
+		entity = productRepository.save(entity);
+		return new ProductDTO(entity);
 	}
 }
