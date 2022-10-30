eb6965c8954060cbe8b5efd1c239e4e4d48868f7
EduardoGomes1982 <edu1982talk@gmail.com>
2022-10-30 20:28:37 -0300

    Search by name for Products

--- backend/src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.devsuperior.dscommerce.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dscommerce.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+	@Query("select obj from Product obj where upper(obj.name) like concat('%', upper(:name), '%')")
+	Page<Product> searchByName(String name, Pageable pageable);
 }
