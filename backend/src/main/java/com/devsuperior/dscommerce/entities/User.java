754aaf69198dbd3c068040e6fdcb2fc62bf049c8
EduardoGomes1982 <edu1982talk@gmail.com>
2022-11-06 11:22:15 -0300

    User Role model

--- backend/src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -2,14 +2,20 @@ package com.devsuperior.dscommerce.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -19,19 +25,23 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
-	
+
 	@Column(unique = true)
 	private String email;
-	
+
 	private String phone;
 	private LocalDate birthDate;
 	private String password;
-	
+
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
-	
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
 	public User() {
 	}
 
@@ -94,6 +104,10 @@ public class User {
 
 	public List<Order> getOrders() {
 		return orders;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
 	}
 
 	@Override
